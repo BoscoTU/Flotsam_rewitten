a0cc1c4b651f1fd3b9457d36e25e05c044f54b81
Esquimalt Atom Smashers <esquimaltatomsmashers@gmail.com>
2025-09-27 16:11:24 -0700

    semi-works: OTOS localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/roadrunner/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/roadrunner/OTOSLocalizer.java
@@ -15,11 +15,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Config
 public class OTOSLocalizer implements Localizer {
     public static class Params {
-        public double angularScalar = 1.0;
-        public double linearScalar = 1.0;
+        public double angularScalar = 0.9932;
+        public double linearScalar = 0.9955085865257596;
 
         // Note: units are in inches and radians
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(-2.9375, -1.9375, 0);
     }
 
     public static Params PARAMS = new Params();
