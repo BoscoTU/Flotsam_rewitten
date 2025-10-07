a541420e0e268d6fb0f91edc6b60d01c9bcc9c59
Esquimalt Atom Smashers <esquimaltatomsmashers@gmail.com>
2025-10-07 09:44:00 -0700

    Update CirclingShiftingAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CirclingShiftingAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CirclingShiftingAuto.java
@@ -22,30 +22,41 @@ public class CirclingShiftingAuto extends LinearOpMode {
     private MecanumDrive mecanumDrive;
     @Override
     public void runOpMode()  {
-        Pose2d beginPose = new Pose2d( 0, 35, Math.toRadians(90));
+        Pose2d beginPose = new Pose2d( 0, 48, Math.toRadians(90));
         this.mecanumDrive = new MecanumDrive(hardwareMap, beginPose);
 
         waitForStart();
 
         Actions.runBlocking(
-                mecanumDrive.actionBuilder(new Pose2d(0, 48, Math.toRadians(90)))
+                mecanumDrive.actionBuilder(beginPose)
                         .strafeToConstantHeading(new Vector2d(0, 54))
                         .setTangent(Math.toRadians(180))
-                        .splineToSplineHeading(new Pose2d(-7.5, 48, Math.toRadians(0)), Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(0, 42, Math.toRadians(90)), Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(7.5, 48, Math.toRadians(180)), Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(0, 54, Math.toRadians(270)), Math.toRadians(180))
+                        .splineToSplineHeading(new Pose2d(-10, 48, Math.toRadians(0)), Math.toRadians(270))
+                        .splineToSplineHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(10, 48, Math.toRadians(180)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(0, 58, Math.toRadians(270)), Math.toRadians(180))
 
-                        .splineToSplineHeading(new Pose2d(-7.5, 48, Math.toRadians(0)), Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(0, 42, Math.toRadians(90)), Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(7.5, 48, Math.toRadians(180)), Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(0, 54, Math.toRadians(270)), Math.toRadians(180))
+                        .splineToSplineHeading(new Pose2d(-10, 48, Math.toRadians(0)), Math.toRadians(270))
+                        .splineToSplineHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(10, 48, Math.toRadians(180)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(0, 58, Math.toRadians(270)), Math.toRadians(180))
 
-                        .splineToSplineHeading(new Pose2d(-7.5, 48, Math.toRadians(0)), Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(0, 42, Math.toRadians(90)), Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(7.5, 48, Math.toRadians(180)), Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(0, 54, Math.toRadians(270)), Math.toRadians(180))
-                        .strafeToConstantHeading(new Vector2d(0, 54))
+                        .splineToSplineHeading(new Pose2d(-10, 48, Math.toRadians(0)), Math.toRadians(270))
+                        .splineToSplineHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(10, 48, Math.toRadians(180)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(0, 58, Math.toRadians(270)), Math.toRadians(180))
+
+                        .splineToSplineHeading(new Pose2d(-10, 48, Math.toRadians(0)), Math.toRadians(270))
+                        .splineToSplineHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(10, 48, Math.toRadians(180)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(0, 58, Math.toRadians(270)), Math.toRadians(180))
+
+                        .splineToSplineHeading(new Pose2d(-10, 48, Math.toRadians(0)), Math.toRadians(270))
+                        .splineToSplineHeading(new Pose2d(0, 38, Math.toRadians(90)), Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(10, 48, Math.toRadians(180)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(0, 58, Math.toRadians(270)), Math.toRadians(180))
+
+                        .strafeToSplineHeading(new Vector2d(0, 48), Math.toRadians(90))
                         .build());
     }
 }
